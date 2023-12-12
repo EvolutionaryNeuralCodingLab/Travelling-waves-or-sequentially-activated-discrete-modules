1bcfd71d3eefb3d46938d01246d44932cda695bf
yuvalorsher <corticodes@gmail.com>
2023-12-12 16:48:56 +0200

    remove seed here it is added outside

--- SimulationAnalysis/analysis_scripts/simulation_table_analysis_script.m
+++ SimulationAnalysis/analysis_scripts/simulation_table_analysis_script.m
@@ -11,13 +11,13 @@ small_scouts_no_filtering_params
 path_to_channel_positions = [get_analysis_code_base_path() 'spatial_data/channel_locations.mat'];
 
 %Base save path (subset grid subfolder will be added automatically
-save_path = [results_dir 'figures/'];
+save_path = ['/media/sil2/Data/Yuval O/Rotem Simulations/New repo tests/figures/'];
 
 % Apply path configurations and load
 [electrode_channel_poitions, cortex, simulation_table, electrode_grid, save_path] = apply_path_configurations_and_load(path_to_channel_positions,cortex_name,path_to_results_table,electrode_grid_name, save_path); 
 
 
 %% Load results and run analysis
-
+rng(0) % Set seed for reproducibility
 simulation_table = simulation_table.two_scouts_results;
 [dip_p_values, PLDC, phase_space_dT, phase_space_dX] = analyze_simulations_from_table(simulation_table, electrode_channel_poitions, electrode_grid, source_type, sampling_rate, cortex, plotting_config, save_path, files_prefix);
