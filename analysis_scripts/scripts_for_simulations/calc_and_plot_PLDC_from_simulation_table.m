b0e9e7689c15c71120d227e559ae551934ca1081
yuvalorsher <corticodes@gmail.com>
2023-12-13 23:43:30 +0200

    Add all analysis files including filtered_vs_non_filtered

---
+++ analysis_scripts/scripts_for_simulations/calc_and_plot_PLDC_from_simulation_table.m
@@ -0,0 +1,39 @@
+clear all
+close all
+
+
+%% Choose relevant params
+large_scouts_params
+% small_scouts_params
+% large_scouts_filtered_params
+
+%% Load parameters (paths inside)
+
+%Channel Positions
+path_to_channel_positions = [get_analysis_code_base_path() 'spatial_data/channel_locations.mat'];
+
+%Base save path (subset grid subfolder will be added automatically
+% save_path = ['/media/sil2/Data/Yuval O/Rotem Simulations/New repo tests/figures/filtered/'];
+save_path = ''; % This prevents saving metrics; but if any plotting option is true it'll save it in current dir
+
+% Apply path configurations and load
+[electrode_channel_poitions, cortex, simulation_table, electrode_grid, save_path] = apply_path_configurations_and_load(path_to_channel_positions,cortex_name,path_to_results_table,electrode_grid_name, save_path); 
+simulation_table = simulation_table.two_scouts_results;
+
+
+%% Load results and run analysis
+rng(0) % Set seed for reproducibility
+[dip_p_values, PLDC, phase_space_dT, phase_space_dX, delta_Ts, delta_Xs] = analyze_simulations_from_table(simulation_table, electrode_channel_poitions, electrode_grid, source_type, sampling_rate, cortex, plotting_config, save_path, files_prefix);
+
+%% Plot PLDC
+
+[all_scouts_dipoles_positions, mean_electrode_dist, scouts_dist_per_electrode_dist, scouts_stds, scouts_dist_per_std] = ...
+    calc_distances(dxs_for_distance, dt, electrode_grid, cortex, simulation_table, electrode_channel_poitions);
+
+%PLDC vs dT - legend is dx
+f=figure;
+plot(delta_Ts,PLDC)
+ylim(plotting_config.PLDC_summary_scale)
+ylabel('PLDC')
+xlabel('\DeltaT / \sigma_T')
+legend(num2str(round(scouts_dist_per_std,1)))
---
+++ analysis_scripts/scripts_for_simulations/filtered_vs_non_filtered_for_large_scouts.m
@@ -0,0 +1,56 @@
+clear all
+close all
+
+%% Plot filtered vs non-filtered electorde signal
+
+large_scouts_filtered_params
+
+%% Load and create signals
+
+%Channel Positions
+path_to_channel_positions = [get_analysis_code_base_path() 'spatial_data/channel_locations.mat'];
+
+% Apply path configurations and load
+[electrode_channel_poitions, cortex, simulation_table, electrode_grid, save_path] = apply_path_configurations_and_load(path_to_channel_positions,cortex_name,path_to_results_table,electrode_grid_name, ''); 
+simulation_table = simulation_table.two_scouts_results;
+
+% Get relevant row and plot head
+row_num = get_row_num_by_dt_dx(simulation_table,dt,dx);
+row = simulation_table(row_num ,:);
+
+if plotting_config.duplicate_hemisphere
+   temp_signal = row.EEG_recordings{1};
+   flipped_electrode_grid = flipud(electrode_grid);
+   temp_signal_flipped = temp_signal;
+   temp_signal_flipped(electrode_grid(:),:) = temp_signal_flipped(flipped_electrode_grid(:),:);
+   temp_signal_duplicated = temp_signal;
+   temp_signal_duplicated(electrode_grid(:),:) = temp_signal(electrode_grid(:),:) + temp_signal_flipped(electrode_grid(:),:);
+   electrode_signals = temp_signal_duplicated;
+else
+   electrode_signals = row.EEG_recordings{1};
+end
+
+%%% Unfiltered signal
+plotting_config.ws = [];
+plotting_config = fill_missing_configs_with_defaults(plotting_config);
+electrode_set_unfiltered = ElectrodeSet(electrode_signals(:,plotting_config.crop_at(1):plotting_config.crop_at(2)), electrode_channel_poitions,plotting_config);
+
+%%% Filtered signal
+plotting_config.ws = [10 12];
+plotting_config.flip_signal = 0;
+plotting_config.pad_samples = 1000;
+plotting_config.n_reps = 100;
+electrode_set_filtered = ElectrodeSet(electrode_signals(:,plotting_config.crop_at(1):plotting_config.crop_at(2)), electrode_channel_poitions,plotting_config);
+
+%% Plot
+
+line_width = 3;
+channels_for_signal = 53;
+f1 = figure;
+t = (1:size(electrode_set_unfiltered.electrode_signals,2)) / plotting_config.fs;
+plot(t,electrode_set_unfiltered.electrode_signals(channels_for_signal,:),'LineWidth',line_width)
+hold on
+plot(t,electrode_set_filtered.electrode_signals(channels_for_signal,:),'LineWidth',line_width)
+legend('Unfiltered signal','Filtered signal')
+xlabel('Time [s]')
+ylabel('Electrode Simulated Signal [AU]')
