abb820776f0678aa13ad40d5acb88c9152b5b1b6
yuvalorsher <corticodes@gmail.com>
2023-12-19 19:45:15 +0200

    Remove desc

--- SimulationAnalysis/simulation_utils/get_crop_at_if_empty.m
+++ SimulationAnalysis/simulation_utils/get_crop_at_if_empty.m
@@ -1,6 +1,4 @@
 function crop_at = get_crop_at_if_empty(crop_at,signal_length)
-%GET_CROP_START_END_IF_EMPTY Summary of this function goes here
-%   Detailed explanation goes here
 if isempty(crop_at)
     crop_at = [1, signal_length];
 end
