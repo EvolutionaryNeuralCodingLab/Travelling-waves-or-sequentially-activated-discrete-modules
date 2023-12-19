6fbce4256b7c86b5c90e3461f601051299cc1bfb
yuvalorsher <corticodes@gmail.com>
2023-12-19 19:29:59 +0200

    Add func desc

--- SimulationAnalysis/simulation_utils/analyze_simulations_from_table.m
+++ SimulationAnalysis/simulation_utils/analyze_simulations_from_table.m
@@ -1,7 +1,7 @@
 function [dip_p_values, PLDCs, phase_space_dT, phase_space_dX, delta_Ts, delta_Xs] = analyze_simulations_from_table(simulation_table, electrode_channel_poitions, electrode_grid, source_type, sampling_rate, cortex, plotting_config, save_path, files_prefix)
-%ANALYZE_SIMULATIONS Summary of this function goes here
+%ANALYZE_SIMULATIONS Analyzes all simulations save in a table format.
 %   Both returns and saves metrics! Unless save_path='' (is_empty) then it
-%   doesn't save
+%   doesn't save.
 
 %sampling_rate introduced later - so throw error if an old script is run,
 %where sampling_rate is actually cortex (the next variable)
