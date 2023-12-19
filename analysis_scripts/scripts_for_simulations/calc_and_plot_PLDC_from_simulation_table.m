708124c1204a92c9ce9e138c73101205165f8628
yuvalorsher <corticodes@gmail.com>
2023-12-19 19:27:20 +0200

    Remove save_path

--- analysis_scripts/scripts_for_simulations/calc_and_plot_PLDC_from_simulation_table.m
+++ analysis_scripts/scripts_for_simulations/calc_and_plot_PLDC_from_simulation_table.m
@@ -2,8 +2,9 @@ clear all
 close all
 
 
-%% Choose relevant params
-large_scouts_params
+%% Uncomment relevant params
+
+large_scouts_params % Main simulation used
 % small_scouts_params
 % large_scouts_filtered_params
 
@@ -13,8 +14,7 @@ large_scouts_params
 path_to_channel_positions = [get_analysis_code_base_path() 'spatial_data/channel_locations.mat'];
 
 %Base save path (subset grid subfolder will be added automatically
-% save_path = ['/media/sil2/Data/Yuval O/Rotem Simulations/New repo tests/figures/filtered/'];
-save_path = ''; % This prevents saving metrics; but if any plotting option is true it'll save it in current dir
+save_path = ''; % Leaing empty prevents saving metrics; if any plotting option is true it'll save it in current dir
 
 % Apply path configurations and load
 [electrode_channel_poitions, cortex, simulation_table, electrode_grid, save_path] = apply_path_configurations_and_load(path_to_channel_positions,cortex_name,path_to_results_table,electrode_grid_name, save_path); 
