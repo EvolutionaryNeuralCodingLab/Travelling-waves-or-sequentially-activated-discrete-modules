a8e0a237b9faa9b5bbe00e848a4781b02b7471c0
yuvalorsher <corticodes@gmail.com>
2023-12-19 19:33:35 +0200

    Add func desc

--- SimulationAnalysis/simulation_utils/fill_missing_configs_with_defaults.m
+++ SimulationAnalysis/simulation_utils/fill_missing_configs_with_defaults.m
@@ -1,6 +1,12 @@
 function filled_plotting_config = fill_missing_configs_with_defaults(plotting_config, default_plotting_config)
-%FILL_MISSING_CONFIGS_WITH_DEFAULTS Summary of this function goes here
-%   Detailed explanation goes here
+%FILL_MISSING_CONFIGS_WITH_DEFAULTS fill plotting_config with defaults
+% defined in default_plotting_config that are missing from plotting_config.
+% If default_plotting_config is not given, get_default_plotting_config() is
+% used.
+%   plotting_config controls the way analyze_simulation_from_table is used.
+%   Along with what plots to create, it controls filtering, noise addition,
+%   etc.
+
 if ~exist('default_plotting_config','var')
     default_plotting_config = get_default_plotting_config();
 end
