a9012c6ec233a22d439349b9d3737dfbce31db3b
yuvalorsher <corticodes@gmail.com>
2023-12-20 20:54:03 +0200

    Add mats and script to plot just final figure

--- analysis_scripts/scripts_for_simulations/calc_and_plot_PLDC_from_simulation_table.m
+++ analysis_scripts/scripts_for_simulations/calc_and_plot_PLDC_from_simulation_table.m
@@ -1,14 +1,35 @@
 clear all
 close all
 
+%% Load final results for figure
 
-%% Uncomment relevant params
+% Large scouts PLDC:
+load('final_mats_for_figures/PLDC_from_simulation_table_large_scouts.mat','delta_Ts','PLDC','plotting_config','scouts_dist_per_std')
 
+% % Small scouts PLDC:
+% load('final_mats_for_figures/PLDC_from_simulation_table_small_scouts.mat','delta_Ts','PLDC','plotting_config','scouts_dist_per_std')
+
+% % Large scouts filteres
+% load('final_mats_for_figures/PLDC_from_simulation_table_large_scouts_filtered.mat','delta_Ts','PLDC','plotting_config','scouts_dist_per_std')
+
+%% Plot PLDC
+
+%PLDC vs dT - legend is dx
+f=figure;
+plot(delta_Ts,PLDC)
+ylim(plotting_config.PLDC_summary_scale)
+ylabel('PLDC')
+xlabel('\DeltaT / \sigma_T')
+legend(num2str(round(scouts_dist_per_std,1)))
+
+%% Calculation
+
+% % Uncomment relevant params
 large_scouts_params % Main simulation used
 % small_scouts_params
 % large_scouts_filtered_params
 
-%% Load parameters (paths inside)
+% % Load parameters (paths inside)
 
 %Channel Positions
 path_to_channel_positions = [get_analysis_code_base_path() 'spatial_data/channel_locations.mat'];
@@ -21,19 +42,11 @@ save_path = ''; % Leaing empty prevents saving metrics; if any plotting option i
 simulation_table = simulation_table.two_scouts_results;
 
 
-%% Load results and run analysis
+% % Load results and run analysis
 rng(0) % Set seed for reproducibility
 [dip_p_values, PLDC, phase_space_dT, phase_space_dX, delta_Ts, delta_Xs] = analyze_simulations_from_table(simulation_table, electrode_channel_poitions, electrode_grid, source_type, sampling_rate, cortex, plotting_config, save_path, files_prefix);
-
-%% Plot PLDC
 
 [all_scouts_dipoles_positions, mean_electrode_dist, scouts_dist_per_electrode_dist, scouts_stds, scouts_dist_per_std] = ...
     calc_distances(dxs_for_distance, dt, electrode_grid, cortex, simulation_table, electrode_channel_poitions);
 
-%PLDC vs dT - legend is dx
-f=figure;
-plot(delta_Ts,PLDC)
-ylim(plotting_config.PLDC_summary_scale)
-ylabel('PLDC')
-xlabel('\DeltaT / \sigma_T')
-legend(num2str(round(scouts_dist_per_std,1)))
+
